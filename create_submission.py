f0cd7b9ff9598bb70b40826548307f790357b0c8
oleksandr <o.l@smoothr.de>
2023-08-25 19:42:01 +0300

    added ensembling to submission

--- create_submission.py
+++ create_submission.py
@@ -9,36 +9,51 @@ from train import CustomDataset, load_model, reversed_groups
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 assert str(device) == 'cuda', 'CUDA is not available'
 
-MODEL_PATH = 'runs/train-08-25_02:53/model.pth'
-model = load_model(from_scratch=False, model_path=MODEL_PATH)
-model = model.to(device)
-model.eval()
-print('Model loaded')
-
 test_data_path = 'data/ukraine-ml-bootcamp-2023/images/test_images'
-submission_csv = os.path.join(os.path.dirname(MODEL_PATH), 'submission.csv')
-
 images = [os.path.join(test_data_path, i) for i in os.listdir(test_data_path)]
+
+
+def get_res(model_path):
+    model_path = os.path.join('runs', model_path, 'model.pth')
+    model = load_model(from_scratch=False, model_path=model_path)
+    model = model.to(device)
+    model.eval()
+    test_data = CustomDataset(images, targets=None, transform=base_transform)
+    data_loader = DataLoader(test_data, batch_size=32, shuffle=False, num_workers=4)
+
+    local_res = []
+    for batch in data_loader:
+        batch = batch.to(device)
+        with torch.no_grad():
+            outputs = model(batch)
+            local_res.extend(outputs)
+
+    assert len(local_res) == len(images), 'Wrong predictions length'
+    tensor_res = torch.cat(local_res)
+    tensor_res = tensor_res.view(len(images), -1)
+    return tensor_res
+
+MODEL_PATHS = [
+    'train-08-25_17:35-WIN',
+    'train-08-25_17:53',
+    ]
+res = []
+
 print('Test size:', len(images))
 
-test_data = CustomDataset(images, targets=None, transform=base_transform)
-data_loader = DataLoader(test_data, batch_size=32, shuffle=False, num_workers=4)
+for i, model_path in enumerate(MODEL_PATHS):
+    res.append(get_res(model_path))
+    print(f'Predictions {i} done')
 
-res = []
-for batch in data_loader:
-    batch = batch.to(device)
-    with torch.no_grad():
-        outputs = model(batch)
-        _, preds = torch.max(outputs, 1)
-        preds = preds.cpu().numpy()
-        grouped_preds = [int(reversed_groups[i]) for i in preds]
-        res.extend(grouped_preds)
+final_res = torch.stack(res).mean(dim=0)
+final_res = [r.argmax().item() for r in final_res]
+final_res = [reversed_groups[r] for r in final_res]
 
-assert len(res) == len(images), 'Wrong predictions length'
 
-print('Predictions done')
-
-with open(submission_csv, 'w') as f:
+submission_name = "submissions/" + '~'.join(MODEL_PATHS) + '~~submission.csv'
+if not os.path.exists('submissions'):
+    os.makedirs('submissions')
+with open(submission_name, 'w') as f:
     f.write('image_id,class_6\n')
-    for image, pred in zip(images, res):
+    for image, pred in zip(images, final_res):
         f.write(f'{os.path.basename(image)},{pred}\n')
