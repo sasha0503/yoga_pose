a5501470aa1cafd0f6a03d981f0ab79e55dc06fa
oleksandr <o.l@smoothr.de>
2023-08-25 19:41:22 +0300

    added normalization to data preprocessing

--- data_transform.py
+++ data_transform.py
@@ -8,6 +8,7 @@ from torchvision import transforms
 base_transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 augmentation_transform = transforms.Compose([
@@ -20,6 +21,7 @@ augmentation_transform = transforms.Compose([
     transforms.RandomResizedCrop(size=(224, 224), scale=(0.8, 1.0)),
     transforms.RandomGrayscale(p=0.1),
     transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 if __name__ == '__main__':
